be29b64321660a5dfb47fe04850b6a7c231c1379
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-05-20 14:26:27 +0400

    Add files via upload

---
+++ formvalidation/assets/js/script.js
@@ -0,0 +1,11 @@
+// function control(formId) {
+//     let invalids=document.getElementById(formId).querySelectorAll(':invalid').length,
+//     button=document.getElementById(formId).querySelector('button[type="submit"]');
+//     console.log(invalids)
+//     if (invalids===0) {
+//         button.removeAttribute('disabled');
+//     } else {
+//         button.setAttribute('disabled','disabled');
+//     }
+//     control('form1');
+// }
